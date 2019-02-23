d2f591e48449f350c79cdc92e80591d54fe57f27
Beckett <k.beckett@uleth.ca>
2019-02-23 14:28:57 -0700

    javascript progress

--- js/script.js
+++ js/script.js
@@ -11,7 +11,11 @@
 // var q9 = "You get up and look at the sausage, there's a bite out of it. At least you got it back though!",
 // var q10 = "Welp, looks like the end of our tale. Good job?",
 
-var question = ["So your family heriloom sausage, cured for 3000 years and slow smoked for another 2000 just to make it sound fancier has been stolen. What do?",
+
+
+// array to store the content of the prompts
+var question = ["Hi " + name + ", we're going to call ya Joe.",
+				"So your family heriloom sausage, cured for 3000 years and slow smoked for another 2000 just to make it sound fancier has been stolen. What do?",
 				"The guards give you the information of the thief who took the sausage: they were the height of a halfling, cloaked in black! They took off to the halfling village in the magical forest!",
 				"You find yourself in the halfling village, the only place that looks open is the tavern.",
 				"Good one, everyone turns to look at you. A halfling cloaked in black flees into the back room.",
@@ -19,14 +23,70 @@ var question = ["So your family heriloom sausage, cured for 3000 years and slow 
 				"The halfling throws the sausage at you. It hits you in the face and you fall over, and the halfling disappears into the darkness of the magic forest",
 				"You get up and look at the sausage, there's a bite out of it. At least you got it back though!",
 				"Welp, looks like the end of our tale. Good job?"];
+
+// array to store the content of the first input option button
+var input1 = ["Cool", 
+			"Look for the thief", 
+			"Search the forest for clues", 
+			"Kick down the door!", 
+			"Chase the halfling!", 
+			"Strike a cool pose and tell them they're busted!", 
+			"Get up", 
+			"Return home",
+			"The"];
+
+// array to store the content of the second input option button
+var input2 = ["Wait, what??",
+				"Tell the guards",
+				"Go to the Halfling Village",
+				"Sneak in the door!",
+				"Try to sneak around back to cut them off",
+				"Try to knock them down",
+				"Inspect the sausage",
+				"Take a contemplative walk in the forest",
+				"End"];
+
+// variable to increment array position
 var counter = 0;
 
+// call init function
 window.onload = init;
+
+//input.addEventListener("onclick", increment);
+
+// this function does the things
 function init() {
-	// console.log(question [3]);
-	console.log(counter);
-	if (document.getElementById("input").onclick == true); {
-		counter++;
-	};
-	document.getElementById("question").innerHTML[counter];
+
+	// prompts the user to input their name
+	var name = prompt("What's your name?", "");
+
+	console.log(question [counter]);
+	console.log(input1 [counter]);
+	console.log(input2 [counter]);
+
+	//if (document.getElementById("input").onclick == true); {
+	//	counter++;
+	//};
+	// document.getElementById("question").innerHTML[counter];
 }
+
+
+document.getElementById("input").addEventListener("click", function() {
+	counter++;
+	console.log(counter);
+	if (counter >= 0 && counter <= 8) {
+		//document.getElementById("question").innerHTML(question [counter]);
+	} else {
+		location.reload(true);
+	};
+
+});
+
+
+//function increment() {
+//	counter++;
+//	console.log(counter);
+//}
+
+//document.getElementById("input").onclick = increment;
+//console.log(counter);
